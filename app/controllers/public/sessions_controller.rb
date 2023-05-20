58a94b2c9935c15c78463e5128e0a85aea1b5c9d
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-05-20 06:10:58 +0000

    [Update]public/customers

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -2,6 +2,7 @@
 
 class Public::SessionsController < Devise::SessionsController
   before_action :configure_sign_in_params, only: [:create]
+  before_action :customer_state, only: [:create]
 
   # GET /resource/sign_in
   # def new
@@ -25,13 +26,11 @@ class Public::SessionsController < Devise::SessionsController
      devise_parameter_sanitizer.permit(:sign_in, keys: [:attribute])
    end
 
-   def reject_user
-     @customer = Customer.find_by(name: params[:user][:name])
-     if @customer
-       if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == false)
-        flash[:notice] = "退会済みです。再度ご登録をしてご利用ください。"
-        redirect_to new_customer_session_path
-       end
-     end
+   def customer_state
+     @customer = Customer.find_by(email: params[:customer][:email])
+     return if !@customer
+      if @customer.valid_password?(params[:customer][:password]) && (@customer.is_deleted == true)
+       redirect_to new_customer_session_path
+      end
    end
 end
