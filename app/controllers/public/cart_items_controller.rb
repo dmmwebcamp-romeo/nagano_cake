55782310cbb74449ac0a7b16d39deae66d222443
OluMel2 <129649026+OluMel2@users.noreply.github.com>
2023-05-21 12:20:08 +0000

    [Add]public/cart_items

---
+++ app/controllers/public/cart_items_controller.rb
@@ -0,0 +1,58 @@
+class Public::CartItemsController < ApplicationController
+before_action :authenticate_customer!
+
+  def index
+    @cart_items = current_customer.cart_items
+    @total = 0
+  end
+
+  def create
+    @cart_item = current_customer.cart_items.new(cart_item_params)
+    if current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id]).present?
+      cart_item = current_customer.cart_items.find_by(item_id: params[:cart_item][:item_id])
+      cart_item.quantity += params[:cart_item][:quantity].to_i
+      cart_item.save
+      redirect_to cart_items_path
+
+    elsif @cart_item.save
+      @cart_items = current_customer.cart_items.all
+      render 'index'
+    else
+      render 'index'
+    end
+
+  end
+
+  def update
+    @cart_item = CartItem.find(params[:id])
+    if @cart_item.update(cart_item_params)
+      redirect_to request.referer
+    else
+      render :index
+    end
+  end
+
+
+
+  def destroy
+    cart_item = CartItem.find(params[:id])
+    cart_item.destroy
+    redirect_to request.referer
+  end
+
+  def destrou_all
+    @cart_items = current_customer.cart_items
+    @cart_items.destroy_all
+    redirect_to request.referer
+  end
+
+
+
+  private
+
+  def cart_item_params
+        params.require(:cart_item).permit(:customer_id, :item_id, :quantity)
+  end
+
+
+end
