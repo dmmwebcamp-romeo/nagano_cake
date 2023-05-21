d0f4c3f1430b92b38ff2f8ba6f490b9681c84e53
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-05-21 12:15:02 +0000

    [Add]admin/items

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,23 @@
 class Item < ApplicationRecord
+
+  belongs_to :genre
+
+  has_one_attached :image
+
+  validates :name, presence:true
+  validates :description, presence:true
+  validates :price, presence:true
+
+  def get_image
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image.variant(resize_to_limit: [200, 200]).processed
+  end
+  
+  def add_tax_sales_price
+    (self.price * 1.10).round
+  end
+
 end
