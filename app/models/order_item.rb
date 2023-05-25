cdaf4d79c0048ea2e6df5d65bbb2241e57774344
EC2 Default User <130027255+satoikokoro@users.noreply.github.com>
2023-05-25 12:03:02 +0000

    [Add]admin/orders

--- app/models/order_item.rb
+++ app/models/order_item.rb
@@ -1,4 +1,15 @@
 class OrderItem < ApplicationRecord
   belongs_to :order
   belongs_to :item
+
+
+  enum making_status:
+  
+   {
+    impossible_work:0,
+    waiting_work:1,
+    working:2,
+    finish:3
+  }
+
 end
